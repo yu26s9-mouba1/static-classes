656eb8f3158c034ccf3da184e5ade2cd8adf29dd
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-06 12:35:01 -0400

    Added a name formatter test class and tested all name formats

--- src/test/java/com/pluralsight/NameFormatterTest.java
+++ src/test/java/com/pluralsight/NameFormatterTest.java
@@ -7,8 +7,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class NameFormatterTest {
 
+        // Test simple first + last
+        @Test
+        public void format_shouldFormatFirstAndLastName() {
 
-    
+            String result = NameFormatter.format("Mel", "Johnson");
+
+            assertEquals("Johnson, Mel", result);
+        }
+
+        // Test full 5-part name
+        @Test
+        public void format_shouldFormatCompleteNameWithPrefixMiddleSuffix() {
+
+            String result = NameFormatter.format(
+                    "Dr.",
+                    "Mel",
+                    "B",
+                    "Johnson",
+                    "PhD"
+            );
+
+            assertEquals("Johnson, Dr. Mel B, PhD", result);
+        }
+
+        // Test full name string with everything
+        @Test
+        public void format_shouldParseFullNameWithPrefixMiddleSuffix() {
+
+            String result = NameFormatter.format("Dr. Mel B Johnson, PhD");
+
+            assertEquals("Johnson, Dr. Mel B, PhD", result);
+        }
+
+        // Test full name string with first + last only
+        @Test
+        public void format_shouldParseSimpleFullName() {
+
+            String result = NameFormatter.format("Mel Johnson");
+
+            assertEquals("Johnson, Mel", result);
+        }
+
+        // Test full name with middle + suffix
+        @Test
+        public void format_shouldParseFullNameWithMiddleAndSuffix() {
+
+            String result = NameFormatter.format("Mel B Johnson, PhD");
+
+            assertEquals("Johnson, Mel B, PhD", result);
+        }
+
 
 
 }
